be8b8a6e557ee719a2096b4d302f8884548d077d
narowsk4 <narowsk4@msu.edu>
2015-10-09 16:08:25 -0400

    First commit test. Added Transportation class

---
+++ City/Transportation.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Transportation.h"
+
+
+CTransportation::CTransportation()
+{
+}
+
+
+CTransportation::~CTransportation()
+{
+}
---
+++ City/Transportation.h
@@ -0,0 +1,8 @@
+#pragma once
+class CTransportation
+{
+public:
+	CTransportation();
+	virtual ~CTransportation();
+};
+
